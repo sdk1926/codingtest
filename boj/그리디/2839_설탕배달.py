5b81c8256c62aa2531ed69d5b08c6d03e5e77d2f
sdk <sdk1926@naver.com>
2021-09-01 09:24:45 +0900

    Add: 2839 설탕배달 풀이

---
+++ boj/그리디/2839_설탕배달.py
@@ -0,0 +1,9 @@
+n = int(input())
+if n >= 3 and (n % 5 == 1 or n % 5 == 3):
+    print((n // 5) + 1)
+elif n >= 8 and (n % 5 == 2 or n % 5 == 4):
+    print((n // 5) + 2)
+elif n % 5 == 0:
+    print(n // 5)
+elif n == 4 or n == 7:
+    print(-1)
